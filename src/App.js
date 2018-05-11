143ca8c0327de0ebe1315a5f9ab9b97e3a77cf2b
msoler18 <msoler735@gmail.com>
2018-05-11 12:43:30 -0500

    Solve Challenge

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      counter: 0,
+      content:""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.content} onChange={this.handleChange} ></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
+  }
+
+  handleChange(event){
+    this.setState({
+      content: event.target.value,
+      counter: this.state.content.length
+    })
   }
 }
 
